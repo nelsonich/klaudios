5aa58adea8059e0900b47c3076ec420b2bc00b51
NelsonXachatryan <nelson.xachatryan@beeoncode.com>
2020-04-07 01:16:18 +0400

    ...

--- database/seeds/GameCategoriesTableSeeder.php
+++ database/seeds/GameCategoriesTableSeeder.php
@@ -23,9 +23,9 @@ class GameCategoriesTableSeeder extends Seeder
                 'name' => 'разминка',
                 'image' => 'warm-up.png',
                 'complexity_id' => '1',
-                'status' => 0,
-                'created_at' => '2020-03-04 09:43:59',
-                'updated_at' => '2020-03-04 09:43:59',
+                'status' => 1,
+                'created_at' => '2020-03-04 10:43:59',
+                'updated_at' => '2020-03-07 17:52:25',
             ),
             1 => 
             array (
@@ -33,9 +33,49 @@ class GameCategoriesTableSeeder extends Seeder
                 'name' => 'четвертый лишний',
                 'image' => 'fourth-extra.png',
                 'complexity_id' => '1',
+                'status' => 1,
+                'created_at' => '2020-03-04 10:43:59',
+                'updated_at' => '2020-03-07 17:52:28',
+            ),
+            2 => 
+            array (
+                'id' => 3,
+                'name' => '3D-мышление',
+                'image' => '20200307173555.png',
+                'complexity_id' => '1',
+                'status' => 1,
+                'created_at' => '2020-03-07 17:35:26',
+                'updated_at' => '2020-03-07 18:35:55',
+            ),
+            3 => 
+            array (
+                'id' => 4,
+                'name' => 'Закономерности',
+                'image' => '20200307173714.png',
+                'complexity_id' => '1',
+                'status' => 1,
+                'created_at' => '2020-03-07 18:10:44',
+                'updated_at' => '2020-03-07 18:37:14',
+            ),
+            4 => 
+            array (
+                'id' => 5,
+                'name' => 'четвертый лишний',
+                'image' => 'warm-up.png',
+                'complexity_id' => '2',
+                'status' => 1,
+                'created_at' => '2020-03-08 16:33:32',
+                'updated_at' => '2020-03-10 20:14:47',
+            ),
+            5 => 
+            array (
+                'id' => 6,
+                'name' => 'Множества',
+                'image' => 'warm-up.png',
+                'complexity_id' => '1',
                 'status' => 0,
-                'created_at' => '2020-03-04 09:43:59',
-                'updated_at' => '2020-03-04 09:43:59',
+                'created_at' => '2020-03-09 18:35:39',
+                'updated_at' => '2020-03-09 18:35:39',
             ),
         ));
         
